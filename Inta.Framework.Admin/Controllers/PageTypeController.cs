aeb52aaa0ed69316a7856e69da7bdd59ef862a7b
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2024-05-10 23:13:16 +0300

    .

---
+++ Inta.Framework.Admin/Controllers/PageTypeController.cs
@@ -0,0 +1,217 @@
+﻿using Inta.Framework.Admin.Models;
+using Inta.Framework.Ado.Net;
+using Inta.Framework.Contract;
+using Inta.Framework.Entity;
+using Inta.Framework.Web.Base;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inta.Framework.Admin.Controllers
+{
+
+    [AuthorizationCheck]
+    public class PageTypeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList(PagingDataListRequest<PageType> request)
+        {
+            List<SqlParameter> Parameters = new List<SqlParameter>();
+            if (string.IsNullOrEmpty(request.Search.Name))
+                Parameters.Add(new SqlParameter { ParameterName = "Name", Value = DBNull.Value });
+            else
+                Parameters.Add(new SqlParameter { ParameterName = "Name", Value = request.Search.Name.ToString() });
+
+            Parameters.Add(new SqlParameter { ParameterName = "IsActive", Value = request.Search.IsActive });
+
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            string sqlQuery = "Select * from PageType where (@Name is null or Name like '%'+@Name+'%') and IsActive=@IsActive order by " + request.OrderColumn + (request.OrderType == PagingDataListOrderType.Ascending ? " asc" : " desc");
+            var data = db.Find<PageType>(sqlQuery, System.Data.CommandType.Text, Parameters);
+            int count = data?.Data?.ToList()?.Count ?? 0;
+
+            var pagingData = data.Data.Skip((Convert.ToInt32(request.ActivePageNumber) - 1) * request.PageRowCount).Take(request.PageRowCount).ToList();
+
+            var selectData = pagingData.Select(s => new
+            {
+                Select = "<input type='checkbox' name='" + s.Id + "' />",
+                Id = s.Id,
+                Name = s.Name,
+                Edit = "<a href='javascript:void(0)' onclick=\"$PagingDataList.AddRecordModal('/PageType/Add','True'," + s.Id.ToString() + ")\">Düzenle</a>",
+                Delete = "<a href='javascript:void(0)' onclick=\"$PagingDataList.DeleteRecordModal('PageTypeList','/PageType/Delete',SearchDataList," + s.Id.ToString() + ")\">Sil</a>"
+            }).ToList();
+
+
+            var result = new ReturnPaginationObject<object>
+            {
+                Data = selectData,
+                ResultType = MessageType.Success,
+                DataCount = data.Data.Count,
+                PageCount = (int)Math.Ceiling((decimal)count / (decimal)(request.PageRowCount))
+            };
+
+            return Json(result);
+        }
+        public ActionResult Active(string ids)
+        {
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            foreach (var item in ids.Split(',').ToList())
+            {
+                var result = db.ExecuteNoneQuery("Update PageType set IsActive=1 where id=" + item, System.Data.CommandType.Text);
+            }
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Passive(string ids)
+        {
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            foreach (var item in ids.Split(',').ToList())
+            {
+                var result = db.ExecuteNoneQuery("Update PageType set IsActive=0 where id=" + item, System.Data.CommandType.Text);
+            }
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Delete(string ids)
+        {
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            foreach (var item in ids.Split(',').ToList())
+            {
+                var result = db.ExecuteNoneQuery("Delete from PageType where id=" + item, System.Data.CommandType.Text);
+            }
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Add(int? id)
+        {
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter { ParameterName = "Id", Value = id });
+
+            if (id == 0)
+                return PartialView("Add", new PageType());
+            else
+            {
+                var model = db.Get<PageType>("select * from PageType where Id=@Id", System.Data.CommandType.Text, parameters);
+
+                return PartialView("Add", model.Data);
+            }
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Save(PageType request, HttpPostedFileBase FileImage)
+        {
+            AuthenticationData authenticationData = new AuthenticationData();
+            ReturnObject<PageType> result = new ReturnObject<PageType>();
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+
+            if (!string.IsNullOrEmpty(request.Code))
+                parameters.Add(new SqlParameter { ParameterName = "Code", Value = request.Code });
+            else
+                parameters.Add(new SqlParameter { ParameterName = "Code", Value = DBNull.Value });
+
+            if (!string.IsNullOrEmpty(request.Name))
+                parameters.Add(new SqlParameter { ParameterName = "Name", Value = request.Name });
+            else
+                parameters.Add(new SqlParameter { ParameterName = "Name", Value = DBNull.Value });
+
+            if (!string.IsNullOrEmpty(request.ControllerName))
+                parameters.Add(new SqlParameter { ParameterName = "ControllerName", Value = request.ControllerName });
+            else
+                parameters.Add(new SqlParameter { ParameterName = "ControllerName", Value = DBNull.Value });
+
+            if (!string.IsNullOrEmpty(request.ActionName))
+                parameters.Add(new SqlParameter { ParameterName = "ActionName", Value = request.ActionName });
+            else
+                parameters.Add(new SqlParameter { ParameterName = "ActionName", Value = DBNull.Value });
+
+            if (!string.IsNullOrEmpty(request.ViewName))
+                parameters.Add(new SqlParameter { ParameterName = "ViewName", Value = request.ViewName });
+            else
+                parameters.Add(new SqlParameter { ParameterName = "ViewName", Value = DBNull.Value });
+
+            parameters.Add(new SqlParameter { ParameterName = "IsExplanationEnabled", Value = request.IsExplanationEnabled });
+            parameters.Add(new SqlParameter { ParameterName = "IsShortExplanationEnabled", Value = request.IsShortExplanationEnabled });
+            parameters.Add(new SqlParameter { ParameterName = "CanContentBeAdded", Value = request.CanContentBeAdded });
+            parameters.Add(new SqlParameter { ParameterName = "IsMenuFirstRecord", Value = request.IsMenuFirstRecord });
+            parameters.Add(new SqlParameter { ParameterName = "IsMenuFirstCategory", Value = request.IsMenuFirstCategory });
+            parameters.Add(new SqlParameter { ParameterName = "IsPagingEnabled", Value = request.IsPagingEnabled });
+            parameters.Add(new SqlParameter { ParameterName = "IsActive", Value = request.IsActive });
+
+
+            if (request.Id == 0)
+            {
+                parameters.Add(new SqlParameter { ParameterName = "RecordDate", Value = DateTime.Now });
+
+                db.ExecuteNoneQuery(@"insert into [PageType]
+                (Code,
+                Name,
+                ControllerName,
+                ActionName,
+                ViewName,
+                IsExplanationEnabled,
+                IsShortExplanationEnabled,
+                CanContentBeAdded,
+                IsMenuFirstRecord,
+                IsMenuFirstCategory,
+                IsPagingEnabled,
+                IsActive) 
+                values
+                (@Code,
+                @Name,
+                @ControllerName,
+                @ActionName,
+                @ViewName,
+                @IsExplanationEnabled,
+                @IsShortExplanationEnabled,
+                @CanContentBeAdded,
+                @IsMenuFirstRecord,
+                @IsMenuFirstCategory,
+                @IsPagingEnabled,
+                @IsActive) ", System.Data.CommandType.Text, parameters);
+
+                return Json(new ReturnObject<PageType>
+                {
+                    Data = request,
+                    ResultType = MessageType.Success
+                });
+            }
+            else
+            {
+                parameters.Add(new SqlParameter { ParameterName = "Id", Value = request.Id });
+
+                db.ExecuteNoneQuery(@"Update [PageType] set 
+                Code=@Code,
+                Name=@Name,
+                ControllerName=@ControllerName,
+                ActionName=@ActionName,
+                ViewName=@ViewName,
+                IsExplanationEnabled=@IsExplanationEnabled,
+                IsShortExplanationEnabled=@IsShortExplanationEnabled,
+                CanContentBeAdded=@CanContentBeAdded,
+                IsMenuFirstRecord=@IsMenuFirstRecord,
+                IsMenuFirstCategory=@IsMenuFirstCategory,   
+                IsPagingEnabled=@IsPagingEnabled,
+                IsActive=@IsActive
+                where Id=@Id", System.Data.CommandType.Text, parameters);
+
+                return Json(new ReturnObject<PageType>
+                {
+                    Data = request,
+                    ResultType = MessageType.Success
+                });
+            }
+        }
+    }
+}
---
+++ Inta.Framework.Entity/PageType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inta.Framework.Entity
+{
+	[Table("PageType")]
+	public class PageType
+	{
+		public PageType()
+		{
+		}
+
+		public int Id { get; set; }
+		public int SystemUserId { get; set; }
+		public string Code { get; set; }
+		public string Name { get; set; }
+		public string ControllerName { get; set; }
+		public string ActionName { get; set; }
+		public string ViewName { get; set; }
+		public bool IsExplanationEnabled { get; set; }
+		public bool IsShortExplanationEnabled { get; set; }
+		public bool CanContentBeAdded { get; set; }
+		public bool IsMenuFirstRecord { get; set; }
+		public bool IsMenuFirstCategory { get; set; }
+		public bool IsPagingEnabled { get; set; }
+		public DateTime RecordDate { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
