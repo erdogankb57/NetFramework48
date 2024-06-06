d978169d2a8452d4132f8f9bcf9485deaa6defc7
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2024-06-06 15:13:19 +0300

    .

--- Inta.Framework.Admin/Models/CategorySearch.cs
+++ Inta.Framework.Admin/Models/CategorySearch.cs
@@ -8,7 +8,7 @@ namespace Inta.Framework.Admin.Models
     public class CategorySearch
     {
         public string Name { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         public int IsActive { get; set; }
     }
 }
