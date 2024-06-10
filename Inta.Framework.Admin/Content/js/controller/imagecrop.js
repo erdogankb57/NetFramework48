31c6da021535d1967d4a39da25923052eb125515
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2024-06-10 17:12:14 +0300

    .

---
+++ Inta.Framework.Admin/Content/js/controller/imagecrop.js
@@ -0,0 +1,91 @@
+﻿
+var CropCoordinat;
+
+var ResizeImage = function (width, height) {
+    var boxWidth = width;
+    var boxHeight = height;
+
+
+    const img = new Image();
+    img.onload = function () {
+        trueWidth = this.width;
+        trueHeight = this.height;
+
+        var imageWidth = trueWidth;
+        var imageHeight = trueHeight;
+
+        $("#imagePreview img").attr("width", this.width);
+        $("#imagePreview img").attr("height", this.height);
+
+
+
+        var oranDurumu = true;
+        if (boxWidth > imageWidth) {
+            boxWidth = imageWidth;
+        }
+
+        if (boxHeight > imageHeight) {
+            boxHeight = imageHeight;
+        }
+
+        var x = imageWidth / 2 - boxWidth / 2;
+        var y = imageHeight / 2 - boxHeight / 2;
+        var x1 = x + boxWidth;
+        var y1 = y + boxHeight;
+
+
+        $("#imagePreview").Jcrop({
+            aspectRatio: oranDurumu ? boxWidth / boxHeight : false,
+            setSelect: [x, y, x1, y1],
+            allowSelect: false,
+            allowMove: true,
+            allowResize: true,
+            fixedSupport: false,
+            //boxWidth: boxWidth,
+            //boxHeight: boxHeight,
+            boxWidth: imageWidth,
+            boxHeight: imageHeight,
+            trueSize: [trueWidth, trueHeight],
+            onSelect: function (c) {
+                CropCoordinat = c;
+            }
+        });
+    }
+    img.src = $("#imagePreview").attr("src");
+
+}
+
+var CropImage = function (id) {
+    var w = CropCoordinat.w;
+    var h = CropCoordinat.h;
+    var x = CropCoordinat.x;
+    var y = CropCoordinat.y;
+
+    var splitImageUrl = $("#" + id).attr("src").split("/");
+
+    $.ajax({
+        url: "/ImageCrop/CropImage",
+        type: "POST",
+        data: { "imageName": splitImageUrl[splitImageUrl.length - 1].split("?")[0], "width": w, "height": h, "x": x, "y": y },
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        success: function (data) {
+            debugger;
+            if (data.ResultMessage == "OK") {
+                console.log(data.ImageUrl);
+                $("#jcrop").html("<img id='imagePreview' src='" + data.ImageUrl + "'/>");
+                ResizeImage($("#Width").val(), $("#Height").val());
+                setTimeout(function () {
+                    showAlert(".popupMessage", "Resim başarıyla croplandı.", "success");
+                }, 100);
+            }
+
+        }, error: function (data) {
+            setTimeout(function () {
+                showAlert(".popupMessage", "Resim croplama sırasında hata oluştu.", "error");
+            }, 100);
+        }
+    });
+
+    console.log(CropCoordinat);
+}
--- Inta.Framework.Admin/Controllers/ImageCropController.cs
+++ Inta.Framework.Admin/Controllers/ImageCropController.cs
@@ -1,7 +1,9 @@
 ﻿using Inta.Framework.Ado.Net;
 using Inta.Framework.Entity;
 using Inta.Framework.Web.Base.Authorization;
+using System;
 using System.Configuration;
+using System.Drawing;
 using System.Web.Mvc;
 
 namespace Inta.Framework.Admin.Controllers
@@ -21,5 +23,43 @@ namespace Inta.Framework.Admin.Controllers
             ViewBag.Height = Request["height"];
             return View();
         }
+
+        public ActionResult CropImage(string imageName, int width, int height, int x, int y)
+        {
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            string imageUrl = "";
+            var generalSettings = db.Get<GeneralSettings>("Select top 1 * from GeneralSettings", System.Data.CommandType.Text);
+            if (generalSettings.Data != null)
+                imageUrl = generalSettings.Data.ImageUploadPath;
+
+            //string imageUrl = HttpContext.Server.MapPath(ConfigurationManager.AppSettings["FileUploadEditor"]);
+
+            // Create a new image at the cropped size
+            Bitmap cropped = new Bitmap(width, height);
+
+            //Load image from file
+            using (Image image = Image.FromFile(imageUrl + imageName))
+            {
+                // Create a Graphics object to do the drawing, *with the new bitmap as the target*
+                using (Graphics g = Graphics.FromImage(cropped))
+                {
+                    // Draw the desired area of the original into the graphics object
+                    g.DrawImage(image, new Rectangle(0, 0, width, height), new Rectangle(x, y, width, height), GraphicsUnit.Pixel);
+                    image.Dispose();
+                    g.Dispose();
+                    // Save the result
+                    cropped.Save(imageUrl + imageName);
+                    cropped.Dispose();
+                }
+            }
+            Guid guide = Guid.NewGuid();
+
+            return Json(new
+            {
+                ResultMessage = "OK",
+                ImageUrl = generalSettings.Data.ImageCdnUrl + imageName + "?d=" + guide.ToString()
+            });
+        }
+
     }
 }
