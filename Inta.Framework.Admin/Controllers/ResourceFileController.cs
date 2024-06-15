704a2c5ed27be7f8abdde452fac203a45e787fc2
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2024-06-15 20:29:57 +0300

    .

---
+++ Inta.Framework.Admin/Controllers/ResourceFileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inta.Framework.Admin.Controllers
+{
+    public class ResourceFileController : Controller
+    {
+
+        //Dosyanın bu şekilde çalıştırılabilmesi için propertiesden embeded resource olarak ayarlanması gerekir.
+        public ActionResult GetJavascriptFile(string file)
+        {
+            var asm = System.Reflection.Assembly.Load("Inta.Framework.Web.Base");
+            var names = asm.GetManifestResourceNames();
+            if (names.Any(a => a == file))
+            {
+                var stream = asm.GetManifestResourceStream(names[0]);
+                return File(stream, "application/x-javascript");
+            }
+            else
+                return HttpNotFound();
+
+            
+        }
+    }
+}
