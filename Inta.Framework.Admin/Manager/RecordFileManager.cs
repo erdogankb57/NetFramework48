85923ed2cf4bf67408b7698ddccc3ed080e29fc0
erdogankb57 <51267624+erdogankb57@users.noreply.github.com>
2025-09-16 14:28:29 +0300

    .

---
+++ Inta.Framework.Admin/Manager/RecordFileManager.cs
@@ -0,0 +1,79 @@
+﻿using Inta.Framework.Ado.Net;
+using Inta.Framework.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Inta.Framework.Web.Manager
+{
+    public class RecordFileManager
+    {
+        /// <summary>
+        /// Banner ve tiplerini döner
+        /// </summary>
+        /// <returns></returns>
+        public List<RecordFileModel> Find(int RecordId)
+        {
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter { ParameterName = "RecordId", Value = RecordId });
+
+            List<RecordFileModel> list = new List<RecordFileModel>();
+            var data = db.Find("select * from RecordFile where RecordId=@RecordId", System.Data.CommandType.Text, parameters);
+            for (int i = 0; i < data.Data.Rows.Count; i++)
+            {
+                list.Add(new RecordFileModel
+                {
+                    Name = data.Data.Rows[i]["Name"].ToString(),
+                    ShortExplanation = data.Data.Rows[i]["ShortExplanation"].ToString(),
+                    Explanation = data.Data.Rows[i]["Explanation"].ToString(),
+                    FileName = data.Data.Rows[i]["FileName"].ToString(),
+                    FileTagName = data.Data.Rows[i]["FileTagName"].ToString(),
+                    FileTitleName = data.Data.Rows[i]["FileTitleName"].ToString(),
+                    TargetId = Convert.ToInt32(data.Data.Rows[i]["TargetId"]),
+                    HomePageStatus = Convert.ToBoolean(data.Data.Rows[i]["HomePageStatus"]),
+                    OrderNumber = Convert.ToInt32(data.Data.Rows[i]["OrderNumber"]),
+                    IsActive = Convert.ToBoolean(data.Data.Rows[i]["IsActive"])
+                });
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Banner ve tiplerini döner
+        /// </summary>
+        /// <returns></returns>
+        public List<RecordFileModel> Get(int Id)
+        {
+            DBLayer db = new DBLayer(ConfigurationManager.ConnectionStrings["DefaultDataContext"].ToString());
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter { ParameterName = "Id", Value = Id });
+
+
+            List<RecordFileModel> list = new List<RecordFileModel>();
+            var data = db.Find("select * from RecordFile where Id=@Id", System.Data.CommandType.Text, parameters);
+            for (int i = 0; i < data.Data.Rows.Count; i++)
+            {
+                list.Add(new RecordFileModel
+                {
+                    Name = data.Data.Rows[i]["Name"].ToString(),
+                    ShortExplanation = data.Data.Rows[i]["ShortExplanation"].ToString(),
+                    Explanation = data.Data.Rows[i]["Explanation"].ToString(),
+                    FileName = data.Data.Rows[i]["FileName"].ToString(),
+                    FileTagName = data.Data.Rows[i]["FileTagName"].ToString(),
+                    FileTitleName = data.Data.Rows[i]["FileTitleName"].ToString(),
+                    TargetId = Convert.ToInt32(data.Data.Rows[i]["TargetId"]),
+                    HomePageStatus = Convert.ToBoolean(data.Data.Rows[i]["HomePageStatus"]),
+                    OrderNumber = Convert.ToInt32(data.Data.Rows[i]["OrderNumber"]),
+                    IsActive = Convert.ToBoolean(data.Data.Rows[i]["IsActive"])
+                });
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Inta.Framework.Admin/Models/RecordFileModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inta.Framework.Web.Models
+{
+    public class RecordFileModel
+    {
+        public string Name { get; set; }
+        public string ShortExplanation { get; set; }
+        public string Explanation { get; set; }
+        public string FileName { get; set; }
+        public string FileTagName { get; set; }
+        public string FileTitleName { get; set; }
+        public int TargetId { get; set; }
+        public bool HomePageStatus { get; set; }
+        public int OrderNumber { get; set; }
+        public bool IsActive { get; set; }
+
+
+    }
+}
